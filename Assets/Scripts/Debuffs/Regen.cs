d101faf8546683149234720a0738ec327a983f70
Archaeon <87585325+Archaeon-7@users.noreply.github.com>
2022-08-25 15:18:16 -0400

    Create Regen.cs
    For self heal spell, and eventually more stuff

---
+++ Assets/Scripts/Debuffs/Regen.cs
@@ -0,0 +1,29 @@
+/*
+ * Description: Heals target 1 each turn
+ * Author: Jack Farmilo
+ * Date: 2022/08/16
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Regen : Debuff 
+{
+    public int timeLeft;
+    public override void TriggerEffect(Mage mageScript)
+    {
+        SpriteRenderer sprt = mageScript.gameObject.GetComponent<SpriteRenderer>();
+        sprt.color = Color.green;
+
+        mageScript.TakeHit(-1);
+
+        timeLeft--;
+     
+
+        if (timeLeft == 0)
+        {
+            remove = true;
+            sprt.color = Color.white;
+        }
+    }
+}
